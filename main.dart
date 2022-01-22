9e3f32163bcf3a90edc76e3288e842dc48688f56
Guilherme Marcondes <guilhermehmarcondes@gmail.com>
2022-01-22 19:51:05 -0300

    agora faz uma soma

--- main.dart
+++ main.dart
@@ -5,10 +5,10 @@ void main(){
 	var pause = '';
 	var conca1 = 'hello ';
 	var conca2 = 'world!';
-	
+	List nums = [1,2];
 	print(conca1 + conca2);
-	
+	print(nums[0] + nums[1]);
 	print('pressione Enter para continuar...');
-
+    //cometario 
 	pause = stdin.readLineSync().toString();
 }
